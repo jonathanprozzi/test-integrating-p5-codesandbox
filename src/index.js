0ca45eff51060ab1c4de259c13cbb842e430da34
Jonathan Prozzi <jonathanprozzi@gmail.com>
2018-12-19 17:27:03 -0500

    Added second sketch as example
    Quick demo for having 2 sketches

--- src/index.js
+++ src/index.js
@@ -3,15 +3,33 @@ import p5 from "p5";
 
 const sketch = new p5(p => {
   p.setup = function() {
-    p.createCanvas(400, 400);
+    p.createCanvas(200, 200);
   };
 
   p.draw = function() {
-    p.background(0);
+    p.background(197, 221, 169);
+    p.noStroke();
     if (p.mouseX > p.width / 2) {
-      p.fill(200, 80, 80);
+      p.fill(255, 166, 118);
     } else {
-      p.fill(80, 160, 80);
+      p.fill(155, 210, 173);
+    }
+    p.ellipse(p.mouseX, p.mouseY, 50, 50);
+  };
+});
+
+const sketchTwo = new p5(p => {
+  p.setup = function() {
+    p.createCanvas(200, 200);
+  };
+
+  p.draw = function() {
+    p.background(197, 221, 169);
+    p.noStroke();
+    if (p.mouseX < p.width / 2) {
+      p.fill(255, 166, 118);
+    } else {
+      p.fill(155, 210, 173);
     }
     p.ellipse(p.mouseX, p.mouseY, 50, 50);
   };
